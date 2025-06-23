6941e53e71cff9c55f4f149bbd9690f8a799a7cc
JonatanRasmussen <115923496+JonatanRasmussen@users.noreply.github.com>
2025-06-24 01:48:17 +0200

    forgot to save main...

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from scrape_all import run_all_scrape_scripts
+from scrape_all import AllInOneScraper
 from csv_creator import CsvCreator
 from website_launch import website_launch_main
 #%%
 if __name__ == "__main__":
     # Scrape all data from DTU's websites and save it to scraped_data folder
     # Note that this uses Selenium Webbrowser
-    run_all_scrape_scripts() # THIS TAKES HOURS. Async url scraping is NOT IMPLEMENTED
+    AllInOneScraper.run_all_scrape_scripts() # THIS TAKES HOURS. Async url scraping is NOT IMPLEMENTED
 
     # Take data from scraped_data folder and clean+format+parse it.
     # Then, save it as csv file to website/static folder
